0ead94daba5527eed15b50a71a864063634b6569
Chris Jenkins <cdpjenkins@gmail.com>
2026-01-15 11:40:49 +0000

    Add H2 database

---
+++ 4-12/users/src/main/java/com/cdpjenkins/users/infrastructure/H2ConsoleConfiguration.java
@@ -0,0 +1,28 @@
+package com.cdpjenkins.users.infrastructure;
+
+import org.h2.tools.Server;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.EventListener;
+
+import java.sql.SQLException;
+
+@Configuration
+@Profile({"!prod & !production"})
+public class H2ConsoleConfiguration {
+    private final String WEB_PORT="8082";
+    private Server webServer;
+
+    @EventListener(ApplicationStartedEvent.class)
+    public void start() throws SQLException {
+        this.webServer = Server.createWebServer("-webPort", WEB_PORT).start();
+    }
+
+    @EventListener(ContextClosedEvent.class)
+    public void stop() {
+        this.webServer.stop();
+    }
+}
