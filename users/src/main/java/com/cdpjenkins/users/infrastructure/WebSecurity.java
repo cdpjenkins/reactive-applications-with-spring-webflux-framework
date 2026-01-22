709395f602b7cd4b2ddf5b63e57e40011dc670f1
Chris Jenkins <cdpjenkins@gmail.com>
2026-01-22 21:51:55 +0000

    Add Spring Security and use access control to deny access to all but one of our endpoints

---
+++ users/src/main/java/com/cdpjenkins/users/infrastructure/WebSecurity.java
@@ -0,0 +1,23 @@
+package com.cdpjenkins.users.infrastructure;
+
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientCodecCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class WebSecurity {
+    @Bean
+    SecurityWebFilterChain httpSecurityFilterChain(ServerHttpSecurity http, WebClientCodecCustomizer exchangeStrategiesCustomizer) {
+        return http.authorizeExchange(exchanges ->
+                        exchanges.pathMatchers(HttpMethod.POST, "/users")
+                                .permitAll()
+                                .anyExchange().authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+}
